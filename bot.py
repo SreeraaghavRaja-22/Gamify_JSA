51ccda2a57a5b2d99c142ce6a45bb23158e58200
MatthewJung14 <Matthewjung14@gmail.com>
2025-12-18 14:24:15 -0500

    Configuration part 2: Google Sheets

--- bot.py
+++ bot.py
@@ -8,4 +8,5 @@ sheet = client.open_by_key(config.SHEET_ID)
 master_roster = sheet.worksheet("Master_Roster")
 attendance_logs = sheet.worksheet("Attendance_Logs")
 
-
+# Should print "Arisa is a bozo"
+print(sheet.worksheet("Master_Roster").row_values(1))
--- sheets/client.py
+++ sheets/client.py
@@ -2,7 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 # Allow R/W operations in google sheets
-# Aloow accessing files in GDrive.
+# Allow accessing files in GDrive.
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
@@ -11,7 +11,7 @@ SCOPES = [
 def get_client():
     # Authorize and return a gspread client
     creds = Credentials.from_service_account_file(
-        "credentials.json", # There is no credentials.json file yet, that is tbd. 
+        "credentials.json", 
         scopes=SCOPES
     )
     client = gspread.authorize(creds)
